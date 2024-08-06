3b29bb304094b4841d317f675363c1b9b045e0dd
bysoyeon <167726398+bysoyeon@users.noreply.github.com>
2024-08-06 13:04:42 +0900

    앤티티 객체를 DTO객체로 변환하는 메소드 정리

--- src/main/java/samdasu/jejuddai/entity/store.java
+++ src/main/java/samdasu/jejuddai/entity/store.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import samdasu.jejuddai.dto.ReviewResponseDTO;
+import samdasu.jejuddai.dto.StoreResponseDTO;
 
 @Entity
+@Getter
 public class store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +21,19 @@ public class store {
     private double latitude;
     private double longitude;
     private String image;
+
+    public StoreResponseDTO toResponseDTO() {
+        return StoreResponseDTO.builder()
+                .id(this.id)
+                .name(this.name)
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .address(this.address)
+                .phone(this.phone)
+                .category(this.category)
+                .image(this.image)
+                .build();
+    }
 
 
 }
