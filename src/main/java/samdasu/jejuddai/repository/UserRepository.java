4b8dd776c69f5339da2831ea19fd7a8e4017d1d9
lky3004me <lky3004me@gmail.com>
2024-08-07 02:08:29 +0900

    oauth complete

--- src/main/java/samdasu/jejuddai/repository/UserRepository.java
+++ src/main/java/samdasu/jejuddai/repository/UserRepository.java
@@ -3,6 +3,7 @@ package samdasu.jejuddai.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import samdasu.jejuddai.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long>{
 }
